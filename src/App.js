f2277699423985430d1891528a5a300789d9a3e4
atzsamuel <samuelatz4661@gmail.com>
2022-04-07 20:51:06 -0600

    test login with Auth0

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useAuth0 } from "@auth0/auth0-react";
+import { LoginButton } from "./components/Login";
+import { LogoutButton } from "./components/Logout";
+import { Profile } from "./components/Profile";
 
 function App() {
+  const isAuthenticated = useAuth0();
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +14,17 @@ function App() {
         <p>
           Editt <code>src/App.js</code> and save to reload.
         </p>
+        {isAuthenticated ? (
+          <>
+            <LoginButton />  <LogoutButton />
+            <Profile />
+          </>
+        ) : (
+          <>
+           <Profile />
+            <LogoutButton />
+          </>
+        )}
         <a
           className="App-link"
           href="https://reactjs.org"
